fc8f0707b6f3dda7a8f4fba58e644bf57fc8b23b
SwapnilDas3056 <swapnildas3056@gmail.com>
2021-03-18 16:25:24 +0530

    Chain created

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var constrainedLog, chain;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -34,7 +36,11 @@ function setup(){
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
+    constrainedLog = new Log(820,50,80,PI/2);
+
     bird = new Bird(100,100);
+
+    chain = new Chain(constrainedLog.body,bird.body);
 
 }
 
@@ -59,6 +65,10 @@ function draw(){
     log4.display();
     log5.display();
 
+    constrainedLog.display();
+
     bird.display();
     platform.display();
+
+    chain.display();
 }
